4f24103418d03271dc37847acfb0190f68ad513a
Wellington Almeida - RPA <wellingtonalmeida@grupoaccord.com.br>
2024-11-21 14:33:43 -0300

    initial commit

---
+++ src/main/java/com/br/featherchat/FeatherchatApplication.java
@@ -0,0 +1,13 @@
+package com.br.featherchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatherchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatherchatApplication.class, args);
+	}
+
+}
